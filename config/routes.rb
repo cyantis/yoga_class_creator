ccf59f7c39ca13469d28af5755d4bc4d638bced2
cyantis <cyantis@gmail.com>
2019-08-14 18:59:09 -0600

    cleans up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   resources :teachers do
     resources :lessons do
-      resources :notes
+      resources :notes, only: [:new, :create, :edit, :update, :destroy]
     end
   end
 
-  resource :sessions
+  resource :sessions, only: [:new, :create, :destroy]
 
   root 'sessions#new'
 end
